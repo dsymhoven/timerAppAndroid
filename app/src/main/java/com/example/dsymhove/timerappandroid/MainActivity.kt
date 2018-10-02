f2a2f5838319bae1e1dbf3129327263161b8b79c
dsymhove <david.symhoven@jambit.com>
2018-10-02 08:00:03 +0200

    changes text from button on click

--- app/src/main/java/com/example/dsymhove/timerappandroid/MainActivity.kt
+++ app/src/main/java/com/example/dsymhove/timerappandroid/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.dsymhove.timerappandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val timerTextView = findViewById<TextView>(R.id.timeTextView)
+        val startStopButton = findViewById<Button>(R.id.startStopButton)
+
+        startStopButton.setOnClickListener {v ->
+            Log.d("onClick", "Got here")
+            if (startStopButton.text == getString(R.string.start)) {
+                startStopButton.setText(R.string.stop)
+            } else {
+                startStopButton.setText(R.string.start)
+            }
+        }
     }
 }
