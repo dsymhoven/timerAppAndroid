65d4ed739e09039ab7560076fa5bfe19c0ae8fa2
dsymhove <david.symhoven@jambit.com>
2018-10-08 07:57:43 +0200

    updates UI each second default timer value for now

--- app/src/main/java/com/example/dsymhove/timerappandroid/MainActivity.kt
+++ app/src/main/java/com/example/dsymhove/timerappandroid/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Message
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,15 +18,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var timer: CountDownTimer
+    private lateinit var timerTextView: TextView
     private var timerState = TimerState.Stopped
     private var secondsRemaining = 5L
+    private var displayValue: Long
+    get() {
+        return displayValue
+    }
+    set(value) {
+        timerTextView.text = value.toString()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val timerTextView = findViewById<TextView>(R.id.timeTextView)
+        timerTextView = findViewById<TextView>(R.id.timeTextView)
         val startStopButton = findViewById<Button>(R.id.startStopButton)
+
+        setupUI()
 
         startStopButton.setOnClickListener {v ->
             Log.d("onClick", "Got here")
@@ -34,9 +45,17 @@ class MainActivity : AppCompatActivity() {
                 val startTime = System.currentTimeMillis()
                 startTimer()
             } else {
-                startStopButton.setText(R.string.start)
+                stopTimer()
             }
         }
+    }
+
+    private fun stopTimer() {
+        startStopButton.setText(R.string.start)
+    }
+
+    private fun setupUI() {
+        displayValue = secondsRemaining
     }
 
     private fun startTimer() {
@@ -44,10 +63,13 @@ class MainActivity : AppCompatActivity() {
         timer = object : CountDownTimer(secondsRemaining * 1000, 1000) {
             override fun onFinish() {
                 Log.d("onFinish", "onFinish")
+                stopTimer()
+                setupUI()
             }
 
             override fun onTick(millisUntilFinished: Long) {
                 Log.d("tick", "tack")
+                displayValue = (millisUntilFinished) / 1000
             }
         }.start()
     }
